6719fe73da227d9fd182321511b3591f405ec9f3
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-07-03 20:02:12 +0700

    event handler pake button onClick

--- 8.latihan memakai Babel di local/public/app.js
+++ 8.latihan memakai Babel di local/public/app.js
@@ -1,4 +1,4 @@
-const root = document.querySelector('#root');
+const root = document.querySelector('#root'); // jam dan css 
 
 function tick() {
   const elemen = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Jam Big Ben di London"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h2", {
@@ -8,11 +8,36 @@ function tick() {
       heigth: '200px',
       backgroundColor: 'blue'
     }
-  }, "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate ullam fuga eaque quae, beatae odit distinctio laboriosam saepe culpa expedita?"));
+  }, "ini ngambil dari warna dari inline css"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h2", {
+    className: "box"
+  }, "ini ngambil warna dari file css"));
   ReactDOM.render(elemen, root);
 }
 
 ;
 setInterval(function () {
   tick();
-}, 1000);
+}, 1000); //akhir jam dan css
+//eventHandler
+
+const eventHandler = document.querySelector('#eventHandler');
+
+function klik(pesan) {
+  alert(pesan);
+}
+
+;
+const tombol =
+/*#__PURE__*/
+// <div>
+//     <button onClick={klik}>
+//         klik aku!
+//     </button>
+//     <br/>
+// </div>;
+React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
+  onClick: function () {
+    klik("aku ilhammmm");
+  }
+}, "klik aku!"), /*#__PURE__*/React.createElement("br", null));
+ReactDOM.render(tombol, eventHandler); //akhir eventHandler
--- 8.latihan memakai Babel di local/src/app.jsx
+++ 8.latihan memakai Babel di local/src/app.jsx
@@ -1,5 +1,6 @@
 const root = document.querySelector('#root');
 
+// jam dan css 
 function tick() {
     const elemen = 
     <div> 
@@ -15,12 +16,15 @@ function tick() {
             heigth:'200px',
             backgroundColor:'blue',
         }}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate ullam fuga eaque quae, beatae odit distinctio laboriosam saepe culpa expedita?
+            ini ngambil dari warna dari inline css
+        </h2>
+        <br/>
+        <h2 className="box">
+            ini ngambil warna dari file css  
         </h2>
     </div> 
     
 
-    
     ;
 ReactDOM.render(elemen, root);
 };
@@ -28,3 +32,31 @@ ReactDOM.render(elemen, root);
 setInterval(function () {
     tick();
 }, 1000);
+//akhir jam dan css
+
+
+
+//eventHandler
+const eventHandler = document.querySelector('#eventHandler');
+function klik(pesan){
+    alert(pesan)
+};
+
+const tombol = 
+    // <div>
+    //     <button onClick={klik}>
+    //         klik aku!
+    //     </button>
+    //     <br/>
+    // </div>;
+    <div>
+        <button onClick={function(){
+            klik("aku ilhammmm")
+        }}>
+            klik aku!
+        </button>
+        <br/>
+    </div>;
+
+ReactDOM.render(tombol,eventHandler);
+//akhir eventHandler
