12ccd6c269d15757fe2fe9d6b7ba505cabac864b
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-07-10 03:53:34 +0700

    React.useEffect

--- 8.latihan memakai Babel di local/public/app.js
+++ 8.latihan memakai Babel di local/public/app.js
@@ -1,64 +1,100 @@
-const root = document.querySelector('#root'); // jam dan css 
+// const root = document.querySelector('#root');
+// // jam dan css 
+// function tick() {
+//     const elemen = 
+//     <div> 
+//         <h1> 
+//             Jam Big Ben di London 
+//         </h1>
+//         <h2 > 
+//             {new Date().toLocaleTimeString()} 
+//         </h2>     
+//         <br/>
+//         <h2 className="kotak" style={{
+//             width:'200px',
+//             heigth:'200px',
+//             backgroundColor:'blue',
+//         }}>
+//             ini ngambil dari warna dari inline css
+//         </h2>
+//         <br/>
+//         <h2 className="box">
+//             ini ngambil warna dari file css  
+//         </h2>
+//     </div> 
+//     ;
+// ReactDOM.render(elemen, root);
+// };
+// setInterval(function () {
+//     tick();
+// }, 1000);
+// //akhir jam dan css
+// //eventHandler
+// const eventHandler = document.querySelector('#eventHandler');
+// function klik(pesan){
+//     alert(pesan)
+// };
+// const tombol = 
+// // <div>
+// //     <button onClick={klik}>
+// //         klik aku!
+//     //     </button>
+//     //     <br/>
+//     // </div>;
+//     <div>
+//         <button onClick={function(){
+//             klik("aku ilhammmm")
+//         }}>
+//             klik aku!
+//         </button>
+//         <br/>
+//     </div>;
+// ReactDOM.render(tombol,eventHandler);
+// //akhir eventHandler
+// //React.useState()
+// const state = document.querySelector('#useState');
+// function Penghitung(){
+//     //React.useState(value, func bawaan)
+//     const state    = React.useState(13); 
+//     const angka     = state[0];
+//     const setAngka  = state[1];
+//     return (  
+//             <>
+//                 <button onClick={function(){setAngka(angka-1)}}>
+//                     -
+//                 </button>
+//                 <span>
+//                     {angka}
+//                 </span>
+//                 <button onClick={function(){setAngka(angka+1)}}>
+//                     +
+//                 </button>
+//             </>
+//             );
+// }
+// ReactDOM.render(<Penghitung/>,state);
+// //akhir React.useState()
+//React.useEffect()  :bisa menjalankan sebelum dirender
+const parentObj = ReactDOM.createRoot(document.getElementById('useEffect'));
 
-function tick() {
-  const elemen = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Jam Big Ben di London"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h2", {
-    className: "kotak",
-    style: {
-      width: '200px',
-      heigth: '200px',
-      backgroundColor: 'blue'
-    }
-  }, "ini ngambil dari warna dari inline css"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h2", {
-    className: "box"
-  }, "ini ngambil warna dari file css"));
-  ReactDOM.render(elemen, root);
-}
+function App() {
+  const [diklik, setDiklik] = React.useState(false);
+  const [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    console.log(document.getElementById('judul-terserah'));
+  }, [diklik]); //bisa ngeprint sebelum di render khusus (func diklik)
 
-;
-setInterval(function () {
-  tick();
-}, 1000); //akhir jam dan css
-//eventHandler
-
-const eventHandler = document.querySelector('#eventHandler');
-
-function klik(pesan) {
-  alert(pesan);
-}
-
-;
-const tombol =
-/*#__PURE__*/
-// <div>
-//     <button onClick={klik}>
-//         klik aku!
-//     </button>
-//     <br/>
-// </div>;
-React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
-  onClick: function () {
-    klik("aku ilhammmm");
-  }
-}, "klik aku!"), /*#__PURE__*/React.createElement("br", null));
-ReactDOM.render(tombol, eventHandler); //akhir eventHandler
-//React.useState()
-
-const state = document.querySelector('#state');
-
-function Penghitung() {
-  //React.useState(value, func bawaan)
-  const state = React.useState(13);
-  const angka = state[0];
-  const setAngka = state[1];
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    id: "judul-terserah"
+  }, "Hello ini judul terserah"), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
-      setAngka(angka - 1);
+      setDiklik(true);
     }
-  }, "-"), /*#__PURE__*/React.createElement("span", null, angka), /*#__PURE__*/React.createElement("button", {
+  }, "klik aku dong"), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
-      setAngka(angka + 1);
+      setCount(count + 1);
     }
   }, "+"));
 }
 
-ReactDOM.render( /*#__PURE__*/React.createElement(Penghitung, null), state); //akhir React.useState()
+parentObj.render( /*#__PURE__*/React.createElement(App, null)); //akhir React.useEffect()
--- 8.latihan memakai Babel di local/src/app.jsx
+++ 8.latihan memakai Babel di local/src/app.jsx
@@ -1,91 +1,127 @@
-const root = document.querySelector('#root');
+// const root = document.querySelector('#root');
 
-// jam dan css 
-function tick() {
-    const elemen = 
-    <div> 
-        <h1> 
-            Jam Big Ben di London 
-        </h1>
-        <h2 > 
-            {new Date().toLocaleTimeString()} 
-        </h2>     
-        <br/>
-        <h2 className="kotak" style={{
-            width:'200px',
-            heigth:'200px',
-            backgroundColor:'blue',
-        }}>
-            ini ngambil dari warna dari inline css
-        </h2>
-        <br/>
-        <h2 className="box">
-            ini ngambil warna dari file css  
-        </h2>
-    </div> 
+// // jam dan css 
+// function tick() {
+//     const elemen = 
+//     <div> 
+//         <h1> 
+//             Jam Big Ben di London 
+//         </h1>
+//         <h2 > 
+//             {new Date().toLocaleTimeString()} 
+//         </h2>     
+//         <br/>
+//         <h2 className="kotak" style={{
+//             width:'200px',
+//             heigth:'200px',
+//             backgroundColor:'blue',
+//         }}>
+//             ini ngambil dari warna dari inline css
+//         </h2>
+//         <br/>
+//         <h2 className="box">
+//             ini ngambil warna dari file css  
+//         </h2>
+//     </div> 
     
 
-    ;
-ReactDOM.render(elemen, root);
-};
+//     ;
+// ReactDOM.render(elemen, root);
+// };
 
-setInterval(function () {
-    tick();
-}, 1000);
-//akhir jam dan css
+// setInterval(function () {
+//     tick();
+// }, 1000);
+// //akhir jam dan css
 
 
 
-//eventHandler
-const eventHandler = document.querySelector('#eventHandler');
-function klik(pesan){
-    alert(pesan)
-};
+// //eventHandler
+// const eventHandler = document.querySelector('#eventHandler');
+// function klik(pesan){
+//     alert(pesan)
+// };
 
-const tombol = 
-// <div>
-//     <button onClick={klik}>
-//         klik aku!
-    //     </button>
-    //     <br/>
-    // </div>;
-    <div>
+// const tombol = 
+// // <div>
+// //     <button onClick={klik}>
+// //         klik aku!
+//     //     </button>
+//     //     <br/>
+//     // </div>;
+//     <div>
+//         <button onClick={function(){
+//             klik("aku ilhammmm")
+//         }}>
+//             klik aku!
+//         </button>
+//         <br/>
+//     </div>;
+
+// ReactDOM.render(tombol,eventHandler);
+// //akhir eventHandler
+
+
+
+// //React.useState()
+// const state = document.querySelector('#useState');
+// function Penghitung(){
+//     //React.useState(value, func bawaan)
+//     const state    = React.useState(13); 
+//     const angka     = state[0];
+//     const setAngka  = state[1];
+
+//     return (  
+//             <>
+//                 <button onClick={function(){setAngka(angka-1)}}>
+//                     -
+//                 </button>
+//                 <span>
+//                     {angka}
+//                 </span>
+//                 <button onClick={function(){setAngka(angka+1)}}>
+//                     +
+//                 </button>
+//             </>
+//             );
+
+// }
+// ReactDOM.render(<Penghitung/>,state);
+
+// //akhir React.useState()
+
+
+//React.useEffect()  :bisa menjalankan sebelum dirender
+const parentObj = ReactDOM.createRoot(document.getElementById('useEffect'));
+function App() {
+    
+    const [diklik, setDiklik] = React.useState(false);
+    const [count, setCount] = React.useState(0);
+
+
+    React.useEffect(()=>{
+        console.log(document.getElementById('judul-terserah'));
+    },[diklik]); //bisa ngeprint sebelum di render khusus (func diklik)
+
+
+
+    return(
+    <> 
+        <h1 id="judul-terserah">Hello ini judul terserah</h1>
         <button onClick={function(){
-            klik("aku ilhammmm")
+            setDiklik(true)
         }}>
-            klik aku!
+        klik aku dong
         </button>
-        <br/>
-    </div>;
+        <button onClick={function(){
+            setCount(count+1)
+        }}>
+        +
+        </button>
+    </>
 
-ReactDOM.render(tombol,eventHandler);
-//akhir eventHandler
-
-
-
-//React.useState()
-const state = document.querySelector('#state');
-function Penghitung(){
-    //React.useState(value, func bawaan)
-    const state    = React.useState(13); 
-    const angka     = state[0];
-    const setAngka  = state[1];
-
-    return (  
-            <>
-                <button onClick={function(){setAngka(angka-1)}}>
-                    -
-                </button>
-                <span>
-                    {angka}
-                </span>
-                <button onClick={function(){setAngka(angka+1)}}>
-                    +
-                </button>
-            </>
-            );
-
+    );
 }
-ReactDOM.render(<Penghitung/>,state);
+parentObj.render(<App/>);
 
-//akhir React.useState()
+//akhir React.useEffect()
