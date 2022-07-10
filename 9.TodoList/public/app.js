6bfdffab160a00defc6dfc175877990cb356a2aa
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-07-11 01:58:11 +0700

    todo app fitur delete

--- 9.TodoList/public/app.js
+++ 9.TodoList/public/app.js
@@ -5,17 +5,38 @@ function App() {
   const [seluruhAktivitas, setSeluruhAktivitas] = React.useState([]);
 
   function tambahAktivitas(e) {
-    e.preventDefault(); //ini tuh sama aja kayak seluruhAktivitas.append(aktivitas)
+    e.preventDefault(); // //ini tuh sama aja kayak seluruhAktivitas.append(aktivitas)
+    // setSeluruhAktivitas([...seluruhAktivitas, aktivitas]); 
 
-    setSeluruhAktivitas([...seluruhAktivitas, aktivitas]); //kosongkan form
+    function generateId() {
+      return Date.now();
+    } //menambah data baru ke list aktivitas
+
+
+    setSeluruhAktivitas([...seluruhAktivitas, {
+      id: generateId(),
+      aktivitas: aktivitas
+    }]); //kosongkan form
 
     setAktivitas('');
   }
 
+  function hapusAktivitas(todoId) {
+    const seluruhAktivitasFiltered = seluruhAktivitas.filter(function (row) {
+      return row.id != todoId;
+    });
+    setSeluruhAktivitas(seluruhAktivitasFiltered);
+  }
+
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "h1 mt-5"
-  }, "To Do List"), /*#__PURE__*/React.createElement("ol", null, seluruhAktivitas.map(function (akt) {
-    return /*#__PURE__*/React.createElement("li", null, akt);
+  }, "To Do List"), /*#__PURE__*/React.createElement("ol", null, seluruhAktivitas.map(function (a) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: a.id
+    }, a.aktivitas, /*#__PURE__*/React.createElement("button", {
+      onClick: hapusAktivitas.bind(this, a.id),
+      className: "badge text-bg-danger"
+    }, "hapus"));
   })), /*#__PURE__*/React.createElement("form", {
     onSubmit: tambahAktivitas
   }, /*#__PURE__*/React.createElement("label", null, "Masukkan aktivitas:"), /*#__PURE__*/React.createElement("input", {
@@ -23,7 +44,8 @@ function App() {
     value: aktivitas,
     onChange: function (e) {
       setAktivitas(e.target.value);
-    }
+    },
+    required: true
   }), /*#__PURE__*/React.createElement("button", {
     type: "submit",
     className: "btn btn-primary ms-2"
--- 9.TodoList/src/app.jsx
+++ 9.TodoList/src/app.jsx
@@ -6,27 +6,52 @@ function App() {
     function tambahAktivitas(e) {
         e.preventDefault();
 
-        //ini tuh sama aja kayak seluruhAktivitas.append(aktivitas)
-        setSeluruhAktivitas([...seluruhAktivitas, aktivitas]); 
+        // //ini tuh sama aja kayak seluruhAktivitas.append(aktivitas)
+        // setSeluruhAktivitas([...seluruhAktivitas, aktivitas]); 
+
+        function generateId(){
+            return Date.now();
+        }
+
+        //menambah data baru ke list aktivitas
+        setSeluruhAktivitas([...seluruhAktivitas,{
+            id:generateId(),
+            aktivitas:aktivitas
+        }]);
 
         //kosongkan form
         setAktivitas('');
+    }
+
+
+
+    function hapusAktivitas(todoId) {
+        const seluruhAktivitasFiltered = seluruhAktivitas.filter(function (row) {
+            return row.id != todoId
+        })
+
+        setSeluruhAktivitas(seluruhAktivitasFiltered);
     }
     return (
 
         <>
             <div className='h1 mt-5'>To Do List</div>
             <ol>
-                {seluruhAktivitas.map(function(akt){
-                    return <li>{akt}</li>
+                {seluruhAktivitas.map(function(a){
+                    return( 
+                        <li key={a.id}>{a.aktivitas}
+                        <button onClick={hapusAktivitas.bind(this, a.id)} className="badge text-bg-danger">hapus</button>
+                        </li>
+                       
+                );
                 })}
             </ol>
             <form onSubmit={tambahAktivitas}>
                 <label>Masukkan aktivitas:</label>
                 <input type='text' value={aktivitas} onChange={function(e){
                     setAktivitas(e.target.value);
-                }}/>
-                <button type='submit' className="btn btn-primary ms-2">tambah</button>
+                }} required/>
+                <button type='submit' className="btn btn-primary ms-2" >tambah</button>
             </form>
         </>
     );
